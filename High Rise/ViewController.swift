2ff1d8bdb6c8f2c05c01d33c41c94dd66141ad36
AZCoder2 <tranalli2@gmail.com>
2017-04-15 23:24:25 -0400

    Add base block and score label

--- High Rise/ViewController.swift
+++ High Rise/ViewController.swift
@@ -25,19 +25,21 @@ import SceneKit
 import SpriteKit
 
 class ViewController: UIViewController {
-  @IBOutlet weak var scnView: SCNView!
-  
-  var scnScene: SCNScene!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  
-    scnScene = SCNScene(named: "HighRise.scnassets/Scenes/GameScene.scn")
-    scnView.scene = scnScene
-  }
-  
-  override var prefersStatusBarHidden: Bool {
-    return true
-  }
-  
+    
+    @IBOutlet weak var scnView: SCNView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var scnScene: SCNScene!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scnScene = SCNScene(named: "HighRise.scnassets/Scenes/GameScene.scn")
+        scnView.scene = scnScene
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
 }
